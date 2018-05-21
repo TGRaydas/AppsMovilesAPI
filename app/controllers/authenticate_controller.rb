8730b48cb14fdcc8aac98273b7ffeda826c60e91
tgraydas <pgrand@miuandes.cl>
2018-05-21 13:02:02 -0400

    Update

--- app/controllers/authenticate_controller.rb
+++ app/controllers/authenticate_controller.rb
@@ -2,8 +2,9 @@ class AuthenticateController < ApplicationController
   def authenticate
     command = User.where(user:params[:email], password:params[:password]).first
     token = command[:token]
+    response.set_header("Authenticate", token)
     if !command.nil?
-      render :json => command[:token]
+      render :json => headers
     else
       render json: { error: "Non-Authorized" }, status: :unauthorized
     end
